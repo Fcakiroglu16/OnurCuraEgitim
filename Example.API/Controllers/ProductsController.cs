1a893adeb713d4226a24f67917d70b87d852e762
fcakiroglu16 <f-cakiroglu@outlook.com>
2026-04-01 10:57:28 +0300

    .

---
+++ Example.API/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example.API.Controllers
+{
+    // Attribute based routing
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //endpoint: GET api/products
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok("kalem 1");
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct()
+        {
+            return NoContent();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct()
+        {
+            return NoContent();
+        }
+
+        [HttpPatch]
+        public IActionResult UpdateProductName()
+        {
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct()
+        {
+            return NoContent();
+        }
+    }
+}
--- Example.API/Program.cs
+++ Example.API/Program.cs
@@ -4,23 +4,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//RESTful => Http protocol => Architectural style  => Json
+
+
+//plugins
+builder.Services.AddControllers(); // MVC Design Pattern
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
 var app = builder.Build();
 
+//Middleware
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.MapScalarApiReference(); // swagger web ui
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
